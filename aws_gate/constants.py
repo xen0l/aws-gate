2005bb5a14293c20bc1403cf923857188470dc0d
Adam Števko <adam.stevko@gmail.com>
2019-09-30 02:48:54 +0200

    Add more constants

--- aws_gate/constants.py
+++ aws_gate/constants.py
@@ -9,6 +9,7 @@ DEFAULT_OS_USER = 'ec2-user'
 DEFAULT_SSH_PORT = 22
 DEFAULT_KEY_ALGORITHM = 'rsa'
 DEFAULT_KEY_SIZE = 2048
+SUPPORTED_KEY_TYPES = ['rsa', 'ed25519']
 
 DEFAULT_GATE_DIR = os.path.expanduser('~/.aws-gate')
 DEFAULT_GATE_CONFIG_PATH = os.path.join(DEFAULT_GATE_DIR, 'config')
