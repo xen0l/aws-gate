32737ca5345d8ddd28f4799f1e70a159094a7c70
Adam Števko <adam.stevko@gmail.com>
2019-10-11 01:36:51 +0200

    Deal with flaky tests

--- tests/unit/test_ssh_common.py
+++ tests/unit/test_ssh_common.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch, mock_open, MagicMock, call
 
-from hypothesis import given, example
+from hypothesis import given, example, settings
 from hypothesis.strategies import text, integers, sampled_from
 
 from aws_gate.constants import DEFAULT_GATE_KEY_PATH
@@ -20,6 +20,7 @@ class TestSSHCommon(unittest.TestCase):
             **{"public_key.return_value": "ssh-rsa ranodombase64string"}
         )
 
+    @settings(deadline=None)
     @given(sampled_from(SUPPORTED_KEY_TYPES), integers(min_value=KEY_MIN_SIZE))
     def test_initialize_key(self, key_type, key_size):
         key = SshKey(key_type=key_type)
@@ -56,6 +57,7 @@ class TestSSHCommon(unittest.TestCase):
         with self.assertRaises(ValueError):
             SshKey(key_path="")
 
+    @settings(deadline=None)
     @given(sampled_from(SUPPORTED_KEY_TYPES))
     def test_initialize_key_as_context_manager(self, key_type):
         with patch("builtins.open", new_callable=mock_open()) as open_mock, patch(
