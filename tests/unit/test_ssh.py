61cbfd22e3c98f6058ef9c2b4ed2fa0a4c6de407
Adam Števko <adam.stevko@gmail.com>
2019-11-08 23:06:54 +0100

    Fix tests

--- tests/unit/test_ssh.py
+++ tests/unit/test_ssh.py
@@ -74,9 +74,9 @@ class TestSSH(unittest.TestCase):
         ), patch("aws_gate.ssh.query_instance", return_value=self.instance_id), patch(
             "aws_gate.ssh.SshSession", return_value=MagicMock()
         ) as ssh_session_mock, patch(
-            "aws_gate.ssh_proxy.SshKey", return_value=self.ssh_key
+            "aws_gate.ssh.SshKey", return_value=self.ssh_key
         ), patch(
-            "aws_gate.ssh_proxy.SshKeyUploader", return_value=MagicMock()
+            "aws_gate.ssh.SshKeyUploader", return_value=MagicMock()
         ), patch(
             "aws_gate.ssh.get_instance_details",
             return_value=self.get_instance_details_response,
