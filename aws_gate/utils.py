47afa0fa773675c1191346cfa2c3381db2b0f744
Morten Brix Pedersen <morten.b.pedersen@in.azets.com>
2021-01-08 14:40:29 +0100

    SIGHUP is not supported on Windows.

--- aws_gate/utils.py
+++ aws_gate/utils.py
@@ -113,7 +113,10 @@ def get_default_region():
 @contextlib.contextmanager
 def deferred_signals(signal_list=None):
     if signal_list is None:
-        signal_list = [signal.SIGHUP, signal.SIGINT, signal.SIGTERM]
+        if platform.system() == "Windows":
+            signal_list = [signal.SIGINT, signal.SIGTERM]
+        else:
+            signal_list = [signal.SIGHUP, signal.SIGINT, signal.SIGTERM]
 
     for deferred_signal in signal_list:
         signal_name = signal.Signals(deferred_signal).name
